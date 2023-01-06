093fdde29b8dd430e9631cb463f1f2e0b5a59cdd
artur <arturnowakowski2015@gmail.com>
2023-01-06 13:22:49 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
   Routes,
   Route, Link
 } from "react-router-dom"; 
-import Home from "./components/Home/Home"; 
+import Home from "./components/Tree/TreeNode"; 
  
 
 
@@ -19,7 +19,7 @@ function App() {
 
 
 
-            <Route exact path="/a/:elem/pagination" element={<Home />} >
+            <Route exact path="/a/:elem/pagination" element={<TreeNode />} >
                
             </Route>
                </Routes>
