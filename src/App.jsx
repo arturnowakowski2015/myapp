6cb07b772db88d79fb5874cc56e85098880170e6
artur <arturnowakowski2015@gmail.com>
2023-01-12 16:53:52 +0100

    url

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,35 @@
-
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route, Link
-} from "react-router-dom";  
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Settings from "./components/Settings/Settings";
-import Selected from './components/Selected/Selected';
-import Select from './components/Different/Select'; 
+import Selected from "./components/Selected/Selected";
+import Select from "./components/Different/Select";
 import Home from "./components/Home/Home";
 import Update from "./components/Different/Update";
- 
+
 import Searching from "./components/Different/Searching";
-import Table from "./components/Table/Table"; 
+import Table from "./components/Table/Table";
 
-function App() {  
-
+function App() {
   return (
-          
-           <Router>
-            <Link to="/a/new/pagination" >start</Link> 
-            <Routes>
-                 <Route exact path="/a/:elem/pagination" element={<Home />} >
-                  <Route path=":id/:ii" element={<Table /> } >
-                      <Route exact path="settings" element={<Settings />} />
-                      <Route path="selected" element={<Selected />} />
-                      <Route path="searchtext" element={<Table />} >
-                          <Route path=":r" element={<Searching /> } />
-                        </Route>
-                      <Route path="url" element={<Select />} />
-                  </Route>
-                  <Route path=":str/:id/:title/:f/edit" element={<Update />} />
-                </Route>
-              </Routes>
-            </Router> 
+    <Router>
+      <Routes>
+        <Route path="/a/new/pagination" element={<Home />} />
+        <Route exact path="/a/:elem/pagination" element={<Home />}>
+          <Route path=":id/:ii" element={<Table />}>
+            <Route exact path="settings" element={<Settings />} />
+            <Route path="selected" element={<Selected />} />
+            <Route path="searchtext" element={<Table />}>
+              <Route path=":r" element={<Searching />} />
+            </Route>
+            <Route path="url" element={<Select />} />
+          </Route>
+          <Route path=":str/:id/:title/:f/edit" element={<Update />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
-
-
-
-
-
-
 
 export default App;
 
